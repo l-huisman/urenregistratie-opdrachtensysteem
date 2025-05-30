0bd312be5185ed151bc492b6299ca5325019cda8
Luke Huisman <huisman.l@yahoo.com>
2025-05-30 09:55:12 +0200

    Add soft deletes and relationships to Task model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,11 +2,32 @@
 
 namespace App\Models;
 
+use Database\Factories\TaskFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Task extends Model
 {
-    /** @use HasFactory<\Database\Factories\TaskFactory> */
-    use HasFactory;
+    /** @use HasFactory<TaskFactory> */
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'user_id',
+        'phase_id',
+        'name',
+        'description',
+        'estimated_hours',
+    ];
+
+    protected function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    protected function phase(): BelongsTo
+    {
+        return $this->belongsTo(Phase::class);
+    }
 }
