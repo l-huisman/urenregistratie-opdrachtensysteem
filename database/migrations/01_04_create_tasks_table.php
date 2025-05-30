102baa4e159905dea87110079fee4269fdb67541
Luke Huisman <huisman.l@yahoo.com>
2025-05-30 09:11:09 +0200

    Add foreign key relationship to phases in tasks table migration

--- database/migrations/01_04_create_tasks_table.php
+++ database/migrations/01_04_create_tasks_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Phase;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,7 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(Phase::class)->constrained()->cascadeOnDelete();
             $table->string('name');
             $table->string('description')->nullable();
             $table->decimal('estimated_hours', 8, 2)->default(0.00);
