88a50ccc14d2a29c203a83f1fcbd0998a5f2d00b
Luke Huisman <huisman.l@yahoo.com>
2025-05-30 08:17:14 +0200

    Refactor price_agreements migration to use foreignIdFor for company_id

--- database/migrations/2025_05_23_081732_create_price_agreements_table.php
+++ database/migrations/2025_05_23_081732_create_price_agreements_table.php
@@ -1,11 +1,11 @@
 <?php
 
+use App\Models\Company;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('price_agreements', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('company_id')->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Company::class);
             $table->date('start_date');
             $table->date('end_date')->nullable();
             $table->decimal('price', 8, 2)->comment('Assuming this is a fixed price for something');
