7ec69ba6a247a5abf6cc6df05aee9965b4cdfa26
Luke Huisman <huisman.l@yahoo.com>
2025-05-30 08:33:05 +0200

    Add projects table migration and model with relationships to company and phases

--- database/migrations/2025_05_30_060926_create_projects_table.php
+++ database/migrations/2025_05_30_060926_create_projects_table.php
@@ -1,11 +1,11 @@
 <?php
 
+use App\Models\Company;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,7 +13,12 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(Company::class)->constrained()->cascadeOnDelete();
+            $table->string('name')->nullable(false);
+            $table->string('description')->nullable(false);
+            $table->enum('type', ['bundle', 'fixed'])->nullable(false)->default('bundle');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
