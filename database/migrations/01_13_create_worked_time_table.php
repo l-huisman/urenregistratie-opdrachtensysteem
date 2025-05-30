122672f7d3f6f3c6935fb8f6430b5fd1f270e77e
Luke Huisman <huisman.l@yahoo.com>
2025-05-30 09:07:49 +0200

    Refactored the naming of the database migrations for better maintainability

---
+++ database/migrations/01_13_create_worked_time_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Models\Phase;
+use App\Models\Project;
+use App\Models\Task;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('worked_time', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Project::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Phase::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Task::class)->constrained()->cascadeOnDelete();
+            $table->text('description');
+            $table->decimal('worked_hours', 8, 2);
+            $table->boolean('billable')->default(true);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('worked_time');
+    }
+};
