15f30b5381d562e4d52baa0ca36b2cecbc8f9654
Luke Huisman <huisman.l@yahoo.com>
2025-05-30 08:40:11 +0200

    Refactor price_agreements migration to remove company foreign key and add budgeted_hours and hourly_rate fields

--- database/migrations/2025_05_23_081732_create_price_agreements_table.php
+++ database/migrations/2025_05_23_081732_create_price_agreements_table.php
@@ -1,33 +1,30 @@
 <?php
+    use Illuminate\Database\Migrations\Migration;
+    use Illuminate\Database\Schema\Blueprint;
+    use Illuminate\Support\Facades\Schema;
 
-use App\Models\Company;
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+    return new class extends Migration {
+        /**
+         * Run the migrations.
+         */
+        public function up(): void
+        {
+            Schema::create('price_agreements', function (Blueprint $table) {
+                $table->id();
+                $table->date('start_date');
+                $table->date('end_date')->nullable();
+                $table->decimal('budgeted_hours');
+                $table->decimal('hourly_rate');
+                $table->timestamps();
+                $table->softDeletes();
+            });
+        }
 
-return new class extends Migration {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('price_agreements', function (Blueprint $table) {
-            $table->id();
-            $table->foreignIdFor(Company::class);
-            $table->date('start_date');
-            $table->date('end_date')->nullable();
-            $table->decimal('price', 8, 2)->comment('Assuming this is a fixed price for something');
-            $table->decimal('rate', 8, 2)->comment('Assuming this is an hourly rate');
-            $table->timestamps();
-            $table->softDeletes();
-        });
-    }
-
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('price_agreements');
-    }
-};
+        /**
+         * Reverse the migrations.
+         */
+        public function down(): void
+        {
+            Schema::dropIfExists('price_agreements');
+        }
+    };
