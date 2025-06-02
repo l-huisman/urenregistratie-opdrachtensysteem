1aea7bcaacc08c53ad33306f10194fc3a59f11da
Luke Huisman <huisman.l@yahoo.com>
2025-06-02 09:42:53 +0200

    Add migration for company_price_agreement table

---
+++ database/migrations/01_15_company_price_agreement_table_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\Company;
+use App\Models\PriceAgreement;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('company_price_agreement', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Company::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(PriceAgreement::class)->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('company_price_agreement');
+    }
+};
