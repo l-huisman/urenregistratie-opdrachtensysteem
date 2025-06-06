339e62c84ed301a7fab7156d27ae3da9473c0e1e
Luke Huisman <huisman.l@yahoo.com>
2025-06-06 10:45:28 +0200

    Add role validation methods to BasePolicy for user access control

--- app/Policies/BasePolicy.php
+++ app/Policies/BasePolicy.php
@@ -2,6 +2,7 @@
 
 namespace App\Policies;
 
+use App\Models\User;
 use App\Policies\Concerns\ChecksOwner;
 use App\Policies\Concerns\ChecksRoles;
 
@@ -17,4 +18,26 @@ class BasePolicy
     {
         //
     }
+
+    protected function isAdministrator(User $user): bool
+    {
+        return $user->role->slug === 'administrator';
+    }
+
+    protected function isManager(User $user): bool
+    {
+        return $user->role->slug === 'manager';
+
+    }
+
+    protected function isSelf(User $user, User $model): bool
+    {
+        return $user->id === $model->id;
+    }
+
+    protected function isAdministratorOrManager(User $user): bool
+    {
+        return in_array($user->role->slug, ['administrator', 'manager']);
+    }
 }
+
