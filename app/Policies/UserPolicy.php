fc518800672557ef663a3bfc7376c7bfa3aad456
Luke Huisman <huisman.l@yahoo.com>
2025-07-23 10:36:45 +0200

    Removed unnecessary comments

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -10,49 +10,31 @@ class UserPolicy
 {
     use ChecksRoles, ChecksSelf;
 
-    /**
-     * Determine whether the user can view any models.
-     */
     public function viewAny(User $user): bool
     {
         return $this->isAdministrator($user);
     }
 
-    /**
-     * Determine whether the user can view the model.
-     */
     public function view(User $user, User $model): bool
     {
         return $this->isAdministrator($user) || $user->is($model);
     }
 
-    /**
-     * Determine whether the user can create models.
-     */
     public function create(User $user): bool
     {
         return $this->isAdministrator($user);
     }
 
-    /**
-     * Determine whether the user can update the model.
-     */
     public function update(User $user, User $model): bool
     {
         return $this->isAdministrator($user) || $user->is($model);
     }
 
-    /**
-     * Determine whether the user can delete the model.
-     */
     public function delete(User $user, User $model): bool
     {
         return $this->isAdministrator($user);
     }
 
-    /**
-     * Determine whether the user can restore the model.
-     */
     public function restore(User $user, User $model): bool
     {
         return $this->isAdministrator($user);
